93b2c04858607044d283887197af84390f5dbd0d
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-04-06 10:15:46 +0200

    [UPD] CORREZIONE DEL PROFESSORE

--- js/script.js
+++ js/script.js
@@ -37,24 +37,110 @@ const teamMembers = [
   }
 ];
 
-// FUNZIONE CHE MI CREA LE CARD 
-let creazioneCard = (componenti) => {
-  let card = `<div class="col">
-                   
-                       <div class="card-immagine">
-                         <img src ="./${componenti.img}" alt="" class="img-fluid">
-                       </div>
-                       <div class="testo-card">
-                         <h3>${componenti.name}</h3>
-                         <div>${componenti.role}</div>
-                         <a href="">${componenti.email}</a>
-                       </div>
-                   
-              </div>`
-  document.getElementById('componentiGruppo').innerHTML += card;
+// RECUPERO GLI ELEMENTI DEL DOM CHE MI SERVONO 
+const teamContainer = document.getElementById('team-container');
+const button = document.getElementById('add-member');
+const form = document.getElementById('form-member');
+const nameField = document.getElementById('name');
+const roleField = document.getElementById('role');
+const emailField = document.getElementById('email');
+const imageField = document.getElementById('img');
+
+// FUNZIONE CHE RENDERIZZA LE CARD
+const renderCards = (members, container) => {
+  // DICHIARO UNA VARIABILE CHE MI CONTERRA' LA STRINGA RAPPRESENTANTE LE CARD IN HTML
+  let cards = '';
+
+  // CICLO L'ARRAY DEI MEMBRI DEL TEAM 
+  for (let i = 0; i < members.length; i++) {
+    const member = members[i];
+
+  // DESTRUTTURO L'ELEMENTO DEL TEAM CHE STO ATTUALMENTE CICLANDO NELLE SUE PROPRIETA'
+  const {name, role, email, img} = member;
+
+  // CREO LA CARD IN JAVASCRIPT
+  cards += `<div class="col-12 col-md-6 col-lg-4">
+                  <div class="team-card">
+                    <div class="card-img">
+                      <img src="${img}" class="img-fluid h-100" alt="">
+                    </div>
+                    <div class="card-content">
+                      <h3 class="text-uppercase">${name}</h3>
+                      <div>${role}</div>
+                      <a href="mailto:${email}">${email}</a>
+                    </div>
+                  </div>
+                </div>`
+  }
+
+  // INSERIRE DINAMICAMENTE NEL DOM IL CONTENUTO DI cards
+  container.innerHTML = cards;
+
 }
 
-for (let i = 0; i < teamMembers.length; i++) {
-  creazioneCard(teamMembers[i]);
+// DEFINISCO LA FUNZIONE CHE AL CLICK DEL PULSANTE MI AGGIUNGE L'ELEMENTO ALL'ARRAY
+const addMember = (members, container) =>{
+// RECUPERIAMO I VALORI INSERITI DAI CAMPI INPUT
+const name = nameField.value;
+const role = roleField.value;
+const email = emailField.value;
+const img = imageField.value;
+
+// CREO UN NUOVO OGGETTO CHE ANDRO' POI AD INSERIRE NELL'ARRAY
+const newMember = {
+
+  // SHORTENED INVECE DI name : name E COSI' VIA 
+  name, 
+  role,
+  email,
+  img
 }
+
+// AGGIUNGE IL NUOVO MEMBRO ALL'INTERNO DELL'ARRAY
+members.push(newMember);
+
+console.log(members);
+
+
+// FUNZIONE CHE RENDERIZZA LE CARD 
+renderCards(members,container);
+}
+
+renderCards(teamMembers,teamContainer);
+
+// DICHIARO UNA VARIABILE CHE MI CONTERRA' LA STRINGA RAPPRESENTANTE LE CARD IN HTML
+let cards = '';
+
+// CICLO L'ARRAY DEI MEMBRI DEL TEAM 
+for (let i = 0; i < teamMembers.length; i++) {
+  const member = teamMembers[i];
+
+// DESTRUTTURO L'ELEMENTO DEL TEAM CHE STO ATTUALMENTE CICLANDO NELLE SUE PROPRIETA'
+const {name, role, email, img} = member;
+
+// CREO LA CARD IN JAVASCRIPT
+cards += `<div class="col-12 col-md-6 col-lg-4">
+                  <div class="team-card">
+                    <div class="card-img">
+                      <img src="${img}" class="img-fluid h-100" alt="">
+                    </div>
+                    <div class="card-content">
+                      <h3 class="text-uppercase">${name}</h3>
+                      <div>${role}</div>
+                      <a href="mailto:${email}">${email}</a>
+                    </div>
+                  </div>
+                </div>`
+}
+
+// INSERIRE DINAMICAMENTE NEL DOM IL CONTENUTO DI cards
+teamContainer.innerHTML = cards;
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  // RICHIAMO addMember
+  addMember(teamMembers, teamContainer);
+})
+
+
 
